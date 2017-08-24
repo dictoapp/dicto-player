fbf0707a503d740b3cfb7913e7db4ddd0092ab0e
Robin de Mourat <robin.demourat@gmail.com>
2017-08-24 11:34:31 +0200

    update build process

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,103 @@
-var webpack = require('webpack');
+const nodeExternals = require('webpack-node-externals');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+const extractSass = new ExtractTextPlugin({
+    // filename: "dist/[name].[contenthash].css",
+    filename: "dist/[name].css",
+    disable: process.env.NODE_ENV === "development"
+});
+
 
 module.exports = {
+  entry: './src/Player',
+  output: {
+    filename: './dist/index.js',
+    libraryExport: "default",
+    library: 'dicto-player',
+    libraryTarget: 'commonjs2'
+  },
+  target: 'node', // in order to ignore built-in modules like path, fs, etc. 
+  externals: [nodeExternals()],
   module: {
-    loaders: [
-      { 
-        test: /\.(csv|gexf)$/, 
-        loader: 'raw-loader' 
+    rules: [
+      {
+          test: /\.css$/,
+          loader: ExtractTextPlugin.extract("style-loader", "css-loader")
       },
       {
-        test: /\.(jpe?g|png|gif|svg)$/i,
-        loaders: [
-            'file?hash=sha512&digest=hex&name=[hash].[ext]',
-            'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false'
-        ]
+          test: /\.scss$/,
+          use: extractSass.extract({
+                use: [{
+                    loader: "css-loader",
+                    // options: {
+                    //   sourceMap: true
+                    // }
+                }, 
+                {
+                    loader: "sass-loader",
+                    options: {
+                      sourceMap: true
+                    }
+                }
+                ],
+                // use style-loader in development 
+                fallback: "style-loader",
+                publicPath: "../",
+            })
       },
       {
-        test: /\.scss$/,
-        loaders: ['style', 'css', 'sass']
+        test: /\.js$/,
+        loaders: ['babel-loader'],
+        exclude: /node_modules\//,
+        include: __dirname
       },
       {
-        test: /\.css$/,
-        loaders: ['style', 'css']
+          test: /\.(woff2?|ttf|otf|eot)$/,
+          exclude: /node_modules/,
+          loader: 'file-loader',
+          options: {
+              name: 'dist/[path][name].[ext]'
+          }
       }
-    ]
+    ],
   },
   plugins: [
-    new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: JSON.stringify('production')
-      }
-    }),
-    // new webpack.optimize.UglifyJsPlugin()
-  ]
-};
+    extractSass
+  ],
+}
+
+// var webpack = require('webpack');
+
+// module.exports = {
+//   module: {
+//     loaders: [
+//       {
+//         test: /\.scss$/,
+//         use: ['style-loader', 'css-loader', 'sass-loader']
+//       },
+//       {
+//         test: /\.css$/,
+//         use: ['style-loader', 'css-loader']
+//       },
+//       { 
+//         test: /\.(csv|gexf)$/, 
+//         loader: 'raw-loader' 
+//       },
+//       {
+//         test: /\.(jpe?g|png|gif|svg)$/i,
+//         use: [
+//             'file?hash=sha512&digest=hex&name=[hash].[ext]',
+//             'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false'
+//         ]
+//       },
+//     ]
+//   },
+//   plugins: [
+//     new webpack.DefinePlugin({
+//       'process.env': {
+//         NODE_ENV: JSON.stringify('production')
+//       }
+//     }),
+//     // new webpack.optimize.UglifyJsPlugin()
+//   ]
+// };
